7b244301c7d3e4c95fd2a732e0aeba1d176fd5c7
Jack Ngo <83608215+j-ngo828@users.noreply.github.com>
2023-02-27 22:37:41 -0500

    Finish 05 sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function (firstNum, secondNum) {
+  if (
+    typeof firstNum !== "number" ||
+    typeof secondNum !== "number" ||
+    firstNum < 0 ||
+    secondNum < 0
+  ) {
+    return "ERROR";
+  }
+  let total = 0;
+  for (
+    let i = Math.min(firstNum, secondNum);
+    i <= Math.max(firstNum, secondNum);
+    ++i
+  ) {
+    total += i;
+  }
+  return total;
 };
 
 // Do not edit below this line
