7c2a228ea0a3149a5c926ed5ea910aa0520b233f
Jack Ngo <83608215+j-ngo828@users.noreply.github.com>
2023-02-27 22:13:30 -0500

    Finish 02 repeateString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (str, n) {
+  if (n < 0) {
+    return "ERROR";
+  }
+  let repeatedStr = "";
+  for (let i = 0; i < n; ++i) {
+    repeatedStr += str;
+  }
+  return repeatedStr;
 };
 
 // Do not edit below this line
