df8cb003a3bea24dc418d0cf48ca07f103ef36fe
Jack Ngo <83608215+j-ngo828@users.noreply.github.com>
2023-03-02 22:10:16 -0500

    Finish 09 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const isAlpha = (ch) =>
+    typeof ch === "string" && ch.length === 1 && /[A-Za-z]/.test(ch);
+  const cleanUp = (str) => {
+    let result = "";
+    for (char of str) {
+      if (isAlpha(char)) {
+        result += char.toLowerCase();
+      }
+    }
+    return result;
+  };
+  const trimmed = cleanUp(str);
+  const trimmedHalfLen = Math.floor(trimmed.length / 2);
+  for (let i = 0; i < trimmedHalfLen; ++i) {
+    if (trimmed[i] !== trimmed[trimmed.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line
