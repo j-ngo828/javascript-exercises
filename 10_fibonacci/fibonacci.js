65590a9ac91d43c5b0a5c0f9d6441ea87d79ebd4
Jack Ngo <83608215+j-ngo828@users.noreply.github.com>
2023-03-02 22:16:47 -0500

    Finish 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+  if (typeof n === "number" && n < 1) return "OOPS";
+  if (typeof n === "string") n = parseInt(n);
+  let first = 1;
+  let second = first;
+  if (n === 1 || n === 2) {
+    return first;
+  }
+  let result = 0;
+  for (let i = 3; i <= n; ++i) {
+    result = first + second;
+    first = second;
+    second = result;
+  }
+  return result;
 };
 
 // Do not edit below this line
