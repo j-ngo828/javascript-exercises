2fc5d19c57f16373ca4c5a509f5c161ebcfdbdd1
Jack Ngo <83608215+j-ngo828@users.noreply.github.com>
2023-03-02 22:38:03 -0500

    Finish 12_findTheOldest
    * Finally, requirements unclear

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  const currentYear = new Date().getFullYear();
+  return people.reduce((prevPerson, curPerson) => {
+    const prevPersonAge =
+      (prevPerson.yearOfDeath ? prevPerson.yearOfDeath : currentYear) -
+      prevPerson.yearOfBirth;
+    const curPersonAge =
+      (curPerson.yearOfDeath ? curPerson.yearOfDeath : currentYear) -
+      curPerson.yearOfBirth;
+    return prevPersonAge > curPersonAge ? prevPerson : curPerson;
+  });
 };
 
 // Do not edit below this line
